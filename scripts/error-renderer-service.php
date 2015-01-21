9c11eb95744ec4353ed6ae89242b006687d36c77
Asao <kamei@workspot.jp>
2015-01-21 14:18:53 +0900

    removed ErrorRenderrer and its scripts; use the one in Tuum\View.

---
+++ scripts/error-renderer-service.php
@@ -0,0 +1,23 @@
+<?php
+
+use Tuum\View\ErrorView;
+use Tuum\Web\App;
+use Tuum\Web\Http\Response;
+
+/** @var App $app */
+
+$view = new ErrorView($app->renderer());
+
+/*
+ * set up error templates.
+ */
+
+// default error template file name.
+$view->default_error_file = 'errors/error';
+
+// error template files for each error status code.
+$view->error_files[Response::HTTP_FORBIDDEN] = 'errors/forbidden';
+$view->error_files[Response::HTTP_NOT_FOUND] = 'errors/not-found';
+
+$app->set( 'error-renderer-service', $view );
+return $view;
