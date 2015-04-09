9efa9766e9d4adb638383588738f6f9c1d25b37a
Asao <kamei@workspot.jp>
2015-04-10 00:17:35 +0900

    introduce simple DataView to manage helpers for data.

---
+++ src/DataView.php
@@ -0,0 +1,60 @@
+<?php
+namespace Tuum\View;
+
+use Psr\Http\Message\UriInterface;
+use Tuum\View\Helper\Data;
+use Tuum\View\Helper\Errors;
+use Tuum\View\Helper\Escape;
+use Tuum\View\Helper\Inputs;
+use Tuum\View\Helper\Message;
+
+/**
+ * Class Value
+ *
+ * @package Tuum\View\DataView
+ *
+ */
+class DataView
+{
+    /**
+     * @var Data
+     */
+    public $data;
+
+    /**
+     * @var Message
+     */
+    public $message;
+
+    /**
+     * @var Inputs
+     */
+    public $inputs;
+
+    /**
+     * @var Errors
+     */
+    public $errors;
+
+    /**
+     * @var UriInterface
+     */
+    public $uri;
+
+    /**
+     * @var callable|Escape
+     */
+    public $escape;
+    
+    /**
+     * @param null|callable $escape
+     */
+    public function __construct($escape = null)
+    {
+        if (is_callable($escape)) {
+            $this->escape = $escape;
+        } else {
+            $this->escape = new Escape();
+        }
+    }
+}
